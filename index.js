5ed40fe86607cea692f995ab00becb4a805136e0
utsav2727 <utsav2727@gmail.com>
2022-01-17 17:54:39 +0530

    initial project setup and db connection

---
+++ index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require('./db');
+const express = require('express')
+
+//connect to Mongo
+connectToMongo();
+
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
